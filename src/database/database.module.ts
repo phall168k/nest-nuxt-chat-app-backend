8d34f6d62872795f4cc7b9f0b6881d948155ddc2
phall168k <phall168k@gmail.com>
2026-08-12 10:32:19 +0700

    feat: config database with typeorm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+                type: 'postgres',
+                host: configService.get<string>('TYPEORM_HOST'),
+                port: configService.get<number>('TYPEORM_PORT'),
+                username: configService.get<string>('TYPEORM_USERNAME'),
+                password: configService.get<string>('TYPEORM_PASSWORD'),
+                database: configService.get<string>('TYPEORM_DATABASE'),
+                autoLoadEntities: true,
+                synchronize: true,
+                logging: true,
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+})
 export class DatabaseModule {}
